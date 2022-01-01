e347b8bda8bdfe1d67456706f5e024f08c3ebd8e
yasin.ahlatci <yasin.ahlatci@cloudnesil.com>
2022-01-02 02:26:27 +0300

    files added

--- bookfinder.py
+++ bookfinder.py
@@ -10,13 +10,13 @@ import pandas as pd
 
 def searchProduct(URL, price):
     class functions():
-        def searchAmazon(self,driver, URL, dataset, data_counter, main_url):
+        def searchAmazon(self,driver, URL, dataset, dataCounter, mainUrl):
             driver.get(URL)
             soupObject = BeautifulSoup(driver.page_source, 'html.parser')
             elements = soupObject.find_all("div", {"data-component-type": "s-search-result"})
             for i in elements:
                 address = i.find("a", "a-link-normal a-text-normal").get("href")
-                address = main_url + address
+                address = mainUrl + address
                 driver.get(address)
                 "FIND ASINs"
                 try:
@@ -41,9 +41,9 @@ def searchProduct(URL, price):
                     "FIND BOOK TYPE"
                     fs = chosenItem.find('span', attrs={"class": "a-button-inner"}).a.text
                     fs = fs.split("\n")[1]
-                    new_linki = chosenItem.find('span', 'olp-new olp-link').a.get("href")
-                    new_linki = main_url + new_linki
-                    driver.get(new_linki)
+                    newLink = chosenItem.find('span', 'olp-new olp-link').a.get("href")
+                    newLink = mainUrl + newLink
+                    driver.get(newLink)
                     newSoupItem = BeautifulSoup(driver.page_source, "html.parser")
                     newSoupItemPrices = newSoupItem.find_all("div", attrs={"class": "a-row a-spacing-mini",
                                                                       "class": "a-row a-spacing-mini olpOffer"})
@@ -57,17 +57,17 @@ def searchProduct(URL, price):
                         except:
                             pass
                         results.append(fiyat)
-                    dataset.loc[data_counter] = {'ASIN': ASIN, 'Cover': fs, 'AmzMin': min(results),
+                    dataset.loc[dataCounter] = {'ASIN': ASIN, 'Cover': fs, 'AmzMin': min(results),
                                                  'AmzMax': max(results), 'AmzOrt': sum(results) / len(results),
-                                                 'EbayMin': None, 'EbayMax': None, 'EbayOrt': None, 'Kar':None}
-                    data_counter += 1
+                                                 'EbayMin': None, 'EbayMax': None, 'EbayOrt': None, 'profit':None}
+                    dataCounter += 1
                 except:
                     pass
             """YENİ SAYFAYA GEÇMEK İÇİN BURASI"""
             try:
-                go_next_page=soupObject.find('li',attrs={'class':'a-last'}).a.get('href')
-                go_next_page=main_url+go_next_page
-                self.searchAmazon(driver, go_next_page, dataset, data_counter, main_url)
+                goNextPage = soupObject.find('li',attrs={'class':'a-last'}).a.get('href')
+                goNextPage = mainUrl+goNextPage
+                self.searchAmazon(driver, goNextPage, dataset, dataCounter, mainUrl)
             except:
                 pass
 
@@ -76,39 +76,39 @@ def searchProduct(URL, price):
         def searchEbay(self, driver, dataset):
             minlist = []
             maxlist = []
-            ortlist = []
+            averageList = []
             for i in dataset['ASIN']:
                 url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=' \
                       '{}&_sacat=0&rt=nc&LH_ItemCondition=3'.format(i)
                 driver.get(url)
-                ebay_soup = BeautifulSoup(driver.page_source, 'html.parser')
-                new_books = ebay_soup.find_all('div', attrs={'class': 's-item__details clearfix'})
-                fiyat_list = []
+                soupObject = BeautifulSoup(driver.page_source, 'html.parser')
+                newBooks = soupObject.find_all('div', attrs={'class': 's-item__details clearfix'})
+                priceList = []
                 for i in range(5):
                     try:
-                        price = float(new_books[i].find('span', attrs={'class': 's-item__price'}).text[1:])
+                        price = float(newBooks[i].find('span', attrs={'class': 's-item__price'}).text[1:])
                         try:
                             ship = float(
-                                new_books[i].find('span', attrs={'class': 's-item__shipping s-item__logisticsCost'})
+                                newBooks[i].find('span', attrs={'class': 's-item__shipping s-item__logisticsCost'})
                                     .text.split(" ")[0][2:])
                             price += ship
                         except:
                             pass
-                        fiyat_list.append(price)
+                        priceList.append(price)
                     except:
                         pass
                 try:
-                    minlist.append(min(fiyat_list))
-                    maxlist.append(max(fiyat_list))
-                    ortlist.append(sum(fiyat_list) / len(fiyat_list))
+                    minlist.append(min(priceList))
+                    maxlist.append(max(priceList))
+                    averageList.append(sum(priceList) / len(priceList))
                 except:
                     minlist.append(float(0))
                     maxlist.append(float(0))
-                    ortlist.append(float(0))
+                    averageList.append(float(0))
 
             dataset['EbayMin'] = minlist
             dataset['EbayMax'] = maxlist
-            dataset['EbayOrt'] = ortlist
+            dataset['EbayOrt'] = averageList
             driver.quit()
             return dataset
 
@@ -125,7 +125,7 @@ def searchProduct(URL, price):
             page1.write(0, 5, "EbayMin")
             page1.write(0, 6, "EbayMax")
             page1.write(0, 7, "EbayOrt")
-            page1.write(0, 8, "NetKar")
+            page1.write(0, 8, "Netprofit")
             for i in dataset.values:
                 if(i[8]>price):
                     page1.write(counter, 0, i[0])
@@ -140,17 +140,17 @@ def searchProduct(URL, price):
                     counter+=1
             wb.close()
 
-        def kar_hesap(self, dataset):
-            """kar=AmzFiyat-RefFee-Vcf-Shipping-Alış-DepoKira"""
-            kar_list=[]
+        def calculateProfit(self, dataset):
+            """profit=AmzFiyat-RefFee-Vcf-Shipping-Alış-DepoKira"""
+            profitList=[]
             for i in dataset.values:
-                kar=i[4]-0.15*i[4]-float(1.8)-float(3)-i[5]-float(1)
-                kar_list.append(kar)
-            dataset['Kar']=kar_list
+                profit=i[4]-0.15*i[4]-float(1.8)-float(3)-i[5]-float(1)
+                profitList.append(profit)
+            dataset['profit']=profitList
             return dataset
 
-    islem = functions()
-    main_url = 'https://www.amazon.com'
+    process = functions()
+    mainUrl = 'https://www.amazon.com'
     results = {'ASIN': [],
                 'Cover': [],
                 'AmzMin': [],
@@ -159,16 +159,16 @@ def searchProduct(URL, price):
                 'EbayMin': [],
                 'EbayMax': [],
                 'EbayOrt': [],
-                'Kar':[]}
+                'profit':[]}
     dataset = pd.DataFrame(results, columns=['ASIN', 'Cover', 'AmzMin', 'AmzMax',
-                                              'AmzOrt', 'EbayMin', 'EbayMax', 'EbayOrt', 'Kar'])
-    data_counter = 0
-    driver=webdriver.Chrome()
-    driver.get(main_url)
-    driver,dataset=islem.searchAmazon(driver, URL, dataset, data_counter, main_url)
-    dataset=islem.searchEbay(driver, dataset)
-    dataset=islem.kar_hesap(dataset)
-    islem.writeOutput(dataset, price)
+                                              'AmzOrt', 'EbayMin', 'EbayMax', 'EbayOrt', 'profit'])
+    dataCounter = 0
+    driver = webdriver.Chrome("./driver/chromedriver.exe")
+    driver.get(mainUrl)
+    driver, dataset = process.searchAmazon(driver, URL, dataset, dataCounter, mainUrl)
+    dataset = process.searchEbay(driver, dataset)
+    dataset = process.calculateProfit(dataset)
+    process.writeOutput(dataset, price)
 ###########################################
 """URL = 'https://www.amazon.com/s?me=A3TDR7PXU58ECM&marketplaceID=ATVPDKIKX0DER'
 price=float(1)
